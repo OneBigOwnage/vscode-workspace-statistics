9040ede99527f7bf9dda8a330f053fdf27c6784c
OneBigOwnage <niekvandenbos@hotmail.com>
2019-08-24 22:38:20 +0200

    Remove boilerplate comments, split up the commands between current file and whole workspace.

--- src/extension.ts
+++ src/extension.ts
@@ -4,28 +4,24 @@ import * as vscode from 'vscode';
 import LineCounter from './LineCounter';
 
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "workspace-statistics" is now active!');
 
 	const lineCounter = new LineCounter();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.show-workspace-statistics', async () => {
-		// The code you place here will be executed every time your command is executed
-
-		let message = `The current document has ${await lineCounter.countWorkspaceFiles()} lines.`;
+	let showWorkspaceStatistics = vscode.commands.registerCommand('extension.show-workspace-statistics', async () => {
+		let message = `The current workspace has ${await lineCounter.countWorkspaceFiles()} lines total.`;
 
 		vscode.window.showInformationMessage(message);
 	});
 
-	context.subscriptions.push(disposable);
+	let showCurrentFileStatistics = vscode.commands.registerCommand('extension.show-file-statistics', async () => {
+		let message = `The current file has ${await lineCounter.countCurrentFile()} lines total.`;
+
+		vscode.window.showInformationMessage(message);
+	});
+
+	context.subscriptions.push(showWorkspaceStatistics);
+	context.subscriptions.push(showCurrentFileStatistics);
 }
 
 // this method is called when your extension is deactivated
