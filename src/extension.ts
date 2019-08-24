900e6cd1487fa27422a36296effb5503dcbdd1ee
Niek <niekvandenbos@hotmail.com>
2019-08-24 21:52:11 +0200

    Use the LineCounter class inside a command.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import LineCounter from './LineCounter';
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,14 +12,17 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "workspace-statistics" is now active!');
 
+	const lineCounter = new LineCounter();
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.show-workspace-statistics', async () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		let message = `The current document has ${await lineCounter.countWorkspaceFiles()} lines.`;
+
+		vscode.window.showInformationMessage(message);
 	});
 
 	context.subscriptions.push(disposable);
