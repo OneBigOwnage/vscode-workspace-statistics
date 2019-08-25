c697aa74f90c02ee01adfdb521a359242e15e72b
OneBigOwnage <niekvandenbos@hotmail.com>
2019-08-25 18:29:09 +0200

    Remove testing commands.

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,17 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import LineCounter from './LineCounter';
-
+import InformationPanel from './InformationPanel';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const lineCounter = new LineCounter();
 
 	let showWorkspaceStatistics = vscode.commands.registerCommand('workspace-statistics.show-workspace-statistics', async () => {
-		let message = `The current workspace has ${await lineCounter.countWorkspaceFiles()} lines total.`;
-
-		vscode.window.showInformationMessage(message);
-	});
-
-	let showCurrentFileStatistics = vscode.commands.registerCommand('workspace-statistics.show-file-statistics', async () => {
-		let message = `The current file has ${await lineCounter.countCurrentFile()} lines total.`;
-
-		vscode.window.showInformationMessage(message);
+		InformationPanel.createOrShowPanel(await lineCounter.getWorkspaceFileInfos());
 	});
 
 	context.subscriptions.push(showWorkspaceStatistics);
-	context.subscriptions.push(showCurrentFileStatistics);
 }
 
 // this method is called when your extension is deactivated
